51bbb0b1e39aa3e3ca059c484301889a61d3ef5b
Amir01020 <amirrodjabov@gmail.com>
2023-09-06 22:20:11 +0500

    first commit

--- main/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class MessageForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    phone = forms.CharField(max_length=20)
-    password = forms.CharField(widget=forms.PasswordInput)
